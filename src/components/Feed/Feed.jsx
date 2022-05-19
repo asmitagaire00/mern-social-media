0543b55e08678d8fbd4acf92dd8e378af251a0bb
asmita <asmita@192-168-1-103.tpgi.com.au>
2022-05-19 20:40:41 +1000

    add feed component

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,13 @@
+import Post from "../Post/Post";
+
+import "./Feed.css";
+
+export default function Feed() {
+  return (
+    <div className="feed">
+      <div className="feed-container">
+        <Post />
+      </div>
+    </div>
+  );
+}
