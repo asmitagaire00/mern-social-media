ed35065b4a941f2dda7bbae4389ef4f76c262b19
Asmita Gaire <asmitagaire00@gmail.com>
2022-05-23 16:05:00 +1000

    add social media image and change readme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // import Home from "./pages/Home/Home";
-// import Profile from "./pages/Profile/Profile";
+import Profile from "./pages/Profile/Profile";
 import "./App.css";
 import "./global.css";
-import Register from "./pages/Register/Register";
+// import Register from "./pages/Register/Register";
 // import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      {/* <Profile /> */}
+      <Profile />
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
     </div>
   );
 }
